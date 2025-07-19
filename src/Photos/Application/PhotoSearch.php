ae834e766727f544ef5cba0bdb3b43ad3365e307
Mark Bellingham <mark.bellingham@protonmail.ch>
2025-07-19 20:26:10 +0100

    Replaced PhotoSearch tests

--- src/Photos/Application/PhotoSearch.php
+++ src/Photos/Application/PhotoSearch.php
@@ -11,6 +11,12 @@ use MyEspacio\Photos\Domain\Repository\PhotoRepositoryInterface;
 
 final class PhotoSearch implements PhotoSearchInterface
 {
+    private const string POPULAR_PHOTOS_KEYWORD = 'most-popular';
+    const array NON_ALBUM_SEARCHES = [
+        self::POPULAR_PHOTOS_KEYWORD
+    ];
+    private const string COMMON_SEARCH_TERMS_DIVISORS = '/[\s\/\-_.,;]+/';
+
     private ?PhotoAlbum $album = null;
     private int $paramCount;
     /** @var array<int, string> */
@@ -33,20 +39,27 @@ final class PhotoSearch implements PhotoSearchInterface
         if ($this->album && $this->paramCount > 0) {
             return $this->searchAlbumPhotos();
         }
+        if (
+            $this->album === null &&
+            in_array($album, self::NON_ALBUM_SEARCHES, true)
+        ) {
+            return $this->popularSearchTypes($album);
+        }
         if ($this->album === null && $this->paramCount > 0) {
-            return $this->popularSearchTypes();
+            return $this->photoRepository->search($this->params);
         }
         return $this->photoRepository->randomSelection();
     }
 
     private function parseSearchParams(?string $requestParams): void
     {
-        $this->params = explode(' ', $requestParams ?? '');
-        $this->params = array_filter($this->params);
+        $this->params = preg_split(
+            pattern: self::COMMON_SEARCH_TERMS_DIVISORS,
+            subject: $requestParams ?? '',
+            limit: -1,
+            flags: PREG_SPLIT_NO_EMPTY
+        );
         $this->paramCount = count($this->params);
-        if ($this->paramCount > 0) {
-            $this->params = array_map(fn($param) => trim($param), $this->params);
-        }
     }
 
     private function isAlbum(?string $album): void
@@ -73,10 +86,10 @@ final class PhotoSearch implements PhotoSearchInterface
         return $this->album;
     }
 
-    private function popularSearchTypes(): PhotoCollection
+    private function popularSearchTypes(?string $searchType): PhotoCollection
     {
-        return match ($this->params[0]) {
-            'most-popular' => $this->photoRepository->topPhotos(),
+        return match ($searchType) {
+            self::POPULAR_PHOTOS_KEYWORD => $this->photoRepository->topPhotos(),
             default => $this->photoRepository->search($this->params)
         };
     }
