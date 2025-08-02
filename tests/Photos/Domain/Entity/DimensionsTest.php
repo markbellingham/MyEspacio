1fe5fe5ed3cd1c08a35c532d100c0586dfab808b
Mark Bellingham <mark.bellingham@protonmail.ch>
2025-08-02 20:43:22 +0100

    Improved tests using data providers

--- tests/Photos/Domain/Entity/DimensionsTest.php
+++ tests/Photos/Domain/Entity/DimensionsTest.php
@@ -6,34 +6,81 @@ namespace Tests\Photos\Domain\Entity;
 
 use MyEspacio\Framework\DataSet;
 use MyEspacio\Photos\Domain\Entity\Dimensions;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 final class DimensionsTest extends TestCase
 {
-    public function testDimensions(): void
-    {
-        $dimensions = new Dimensions(456, 123);
+    /** @param array<string, int> $jsonSerialized */
+    #[DataProvider('dimensionsDataProvider')]
+    public function testDimensions(
+        int $width,
+        int $height,
+        array $jsonSerialized,
+    ): void {
+        $dimensions = new Dimensions($width, $height);
 
-        $this->assertSame(456, $dimensions->getWidth());
-        $this->assertSame(123, $dimensions->getHeight());
-        $this->assertEquals(
-            [
-                'width' => 456,
-                'height' => 123
-            ],
-            $dimensions->jsonSerialize()
-        );
+        $this->assertSame($width, $dimensions->getWidth());
+        $this->assertSame($height, $dimensions->getHeight());
+        $this->assertEquals($jsonSerialized, $dimensions->jsonSerialize());
     }
 
-    public function testCreateFromDataset(): void
+    /** @return array<string, array<string, mixed>> */
+    public static function dimensionsDataProvider(): array
     {
-        $data = new DataSet([
-            'width' => 123,
-            'height' => 456
-        ]);
-        $dimensions = Dimensions::createFromDataSet($data);
-        $this->assertInstanceOf(Dimensions::class, $dimensions);
-        $this->assertSame(123, $dimensions->getWidth());
-        $this->assertSame(456, $dimensions->getHeight());
+        return [
+            'test_1' => [
+                'width' => 123,
+                'height' => 456,
+                'jsonSerialized' => [
+                    'width' => 123,
+                    'height' => 456,
+                ],
+            ],
+            'test_2' => [
+                'width' => 321,
+                'height' => 654,
+                'jsonSerialized' => [
+                    'width' => 321,
+                    'height' => 654,
+                ],
+            ]
+        ];
+    }
+
+    #[DataProvider('createFromDatasetDataProvider')]
+    public function testCreateFromDataset(
+        DataSet $dataset,
+        Dimensions $expectedModel,
+    ): void {
+        $dimensions = Dimensions::createFromDataSet($dataset);
+        $this->assertEquals($expectedModel, $dimensions);
+    }
+
+    /** @return array<string, array<string, mixed>> */
+    public static function createFromDatasetDataProvider(): array
+    {
+        return [
+            'test_1' => [
+                'dataset' => new DataSet([
+                    'width' => 123,
+                    'height' => 456,
+                ]),
+                'expectedModel' => new Dimensions(
+                    width: 123,
+                    height: 456,
+                ),
+            ],
+            'test_2' => [
+                'dataset' => new DataSet([
+                    'width' => 321,
+                    'height' => 654,
+                ]),
+                'expectedModel' => new Dimensions(
+                    width: 321,
+                    height: 654,
+                ),
+            ],
+        ];
     }
 }
--- tests/Photos/Domain/Entity/GeoCoordinatesTest.php
+++ tests/Photos/Domain/Entity/GeoCoordinatesTest.php
@@ -6,53 +6,115 @@ namespace Tests\Photos\Domain\Entity;
 
 use MyEspacio\Framework\DataSet;
 use MyEspacio\Photos\Domain\Entity\GeoCoordinates;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
 
 final class GeoCoordinatesTest extends TestCase
 {
-    public function testGeoCoordinates(): void
-    {
+    /** @param array<string, int> $jsonSerialized */
+    #[DataProvider('geoCoordinatesDataProvider')]
+    public function testGeoCoordinates(
+        int $id,
+        UuidInterface $photoUuid,
+        int $latitude,
+        int $longitude,
+        int $accuracy,
+        array $jsonSerialized,
+    ): void {
         $geo = new GeoCoordinates(
-            id: 17,
-            photoUuid: Uuid::fromString('95e7a3b0-6b8a-41bc-bbe2-4efcea215aea'),
-            latitude: 32240065,
-            longitude: 77187860,
-            accuracy:  16
+            $id,
+            $photoUuid,
+            $latitude,
+            $longitude,
+            $accuracy
         );
 
-        $this->assertSame(17, $geo->getId());
-        $this->assertSame('95e7a3b0-6b8a-41bc-bbe2-4efcea215aea', $geo->getPhotoUuid()->toString());
-        $this->assertSame(32240065, $geo->getLatitude());
-        $this->assertSame(77187860, $geo->getLongitude());
-        $this->assertSame(16, $geo->getAccuracy());
-        $this->assertEquals(
-            [
-                'latitude' => 32240065,
-                'longitude' => 77187860,
-                'accuracy' => 16
-            ],
-            $geo->jsonSerialize()
-        );
+        $this->assertSame($id, $geo->getId());
+        $this->assertSame($photoUuid, $geo->getPhotoUuid());
+        $this->assertSame($latitude, $geo->getLatitude());
+        $this->assertSame($longitude, $geo->getLongitude());
+        $this->assertSame($accuracy, $geo->getAccuracy());
+        $this->assertEquals($jsonSerialized, $geo->jsonSerialize());
     }
 
-    public function testCreateFromDataset(): void
+    /** @return array<string, array<string, mixed>> */
+    public static function geoCoordinatesDataProvider(): array
     {
-        $data = new DataSet([
-            'geo_id' => '17',
-            'photo_id' => '17',
-            'photo_uuid' => '95e7a3b0-6b8a-41bc-bbe2-4efcea215aea',
-            'latitude' => '32240065',
-            'longitude' => '77187860',
-            'accuracy' => '16'
-        ]);
+        return [
+            'test_1' => [
+                'id' => 17,
+                'photoUuid' => Uuid::fromString('95e7a3b0-6b8a-41bc-bbe2-4efcea215aea'),
+                'latitude' => 32240065,
+                'longitude' => 77187860,
+                'accuracy' => 16,
+                'jsonSerialized' => [
+                    'latitude' => 32240065,
+                    'longitude' => 77187860,
+                    'accuracy' => 16,
+                ],
+            ],
+            'test_2' => [
+                'id' => 31,
+                'photoUuid' => Uuid::fromString('aa91e0a8-31c7-4ad8-8fa7-1bec70a80d82'),
+                'latitude' => 32309405,
+                'longitude' => 77175694,
+                'accuracy' => 14,
+                'jsonSerialized' => [
+                    'latitude' => 32309405,
+                    'longitude' => 77175694,
+                    'accuracy' => 14,
+                ]
+            ]
+        ];
+    }
 
-        $geo = GeoCoordinates::createFromDataSet($data);
-        $this->assertInstanceOf(GeoCoordinates::class, $geo);
-        $this->assertSame(17, $geo->getId());
-        $this->assertSame('95e7a3b0-6b8a-41bc-bbe2-4efcea215aea', $geo->getPhotoUuid()->toString());
-        $this->assertSame(32240065, $geo->getLatitude());
-        $this->assertSame(77187860, $geo->getLongitude());
-        $this->assertSame(16, $geo->getAccuracy());
+    #[DataProvider('createFromDatasetDataProvider')]
+    public function testCreateFromDataset(
+        DataSet $dataset,
+        GeoCoordinates $expectedModel,
+    ): void {
+        $geo = GeoCoordinates::createFromDataSet($dataset);
+        $this->assertEquals($expectedModel, $geo);
+    }
+
+    /** @return array<string, array<string, mixed>> */
+    public static function createFromDatasetDataProvider(): array
+    {
+        return [
+            'test_1' => [
+                'dataset' => new DataSet([
+                    'geo_id' => '17',
+                    'photo_uuid' => '95e7a3b0-6b8a-41bc-bbe2-4efcea215aea',
+                    'latitude' => '32240065',
+                    'longitude' => '77187860',
+                    'accuracy' => '16',
+                ]),
+                'expectedModel' => new GeoCoordinates(
+                    id: 17,
+                    photoUuid: Uuid::fromString('95e7a3b0-6b8a-41bc-bbe2-4efcea215aea'),
+                    latitude: 32240065,
+                    longitude: 77187860,
+                    accuracy: 16,
+                ),
+            ],
+            'test_2' => [
+                'dataset' => new DataSet([
+                    'geo_id' => '31',
+                    'photo_uuid' => 'aa91e0a8-31c7-4ad8-8fa7-1bec70a80d82',
+                    'latitude' => '32309405',
+                    'longitude' => '77175694',
+                    'accuracy' => '14',
+                ]),
+                'expectedModel' => new GeoCoordinates(
+                    id: 31,
+                    photoUuid: Uuid::fromString('aa91e0a8-31c7-4ad8-8fa7-1bec70a80d82'),
+                    latitude: 32309405,
+                    longitude: 77175694,
+                    accuracy: 14,
+                ),
+            ],
+        ];
     }
 }
